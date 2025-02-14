572ddc50dc9a18c82e638c9afbb5824dcb206b80
kurudoqqe <vadim.firedog@gmail.com>
2025-02-14 16:57:07 +0300

    initialize accordion component

---
+++ src/lib/ui/Accordion/Accordion.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    type Props = {
+        content: string,
+    }
+
+    const {
+        content,
+    }: Props = $props()
+
+</script>
+
+<button class="accordion title-1">{content}</button>
+
+<style lang="scss">
+  @use "sass:map";
+  @use "$lib/ui/env";
+
+    .accordion {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      width: 100%;
+
+      padding: 1.5rem;
+
+      background-color: map.get(env.$bg-color, primary);
+
+      border: 1px solid rgba(map.get(env.$color, primary), 0.1);
+      border-radius: 20px;
+
+      font-family: "Gilroy", sans-serif;
+      font-weight: 600;
+      line-height: 36px;
+    }
+</style>
---
+++ src/routes/_parts/QuestionAnswerSection.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+import Accordion from "$ui-kit/Accordion/Accordion.svelte";
+</script>
+
+<section class="page-container">
+    <h2>Вопросы и ответы</h2>
+    <div class="accordion-container">
+        <Accordion content="Записываться у вас в медицинском центре дороже, чем в клинике?"/>
+        <Accordion content="Как записаться на приём?"/>
+        <Accordion content="Сколько стоит услуги медицинского сервиса?"/>
+        <Accordion content="Как оплачивается приём врача?"/>
+        <Accordion content="Ваш медицинский сервис — клиника?"/>
+    </div>
+</section>
+
+<style lang="scss">
+    .accordion-container {
+      margin-top: 64px;
+
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+</style>
