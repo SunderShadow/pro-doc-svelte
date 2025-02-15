214121f1f70da47b30f09f67a86b3befd6d38e95
kurudoqqe <vadim.firedog@gmail.com>
2025-02-15 15:18:33 +0300

    initialize client section

---
+++ src/routes/_parts/ClientReview.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    type Props = {
+        title: string,
+        description: string,
+        author: string,
+        authorImage: string,
+        rating: number,
+    }
+
+    const {
+        title,
+        description,
+        author,
+        authorImage,
+        rating
+    }: Props = $props()
+
+</script>
+
+<div class="review-container">
+    <h1>{title}</h1>
+    <p>{description}</p>
+    <div class="author">
+        <img src={authorImage} alt="">
+        <p>{author}</p>
+        {rating}
+    </div>
+</div>
+
+<style lang="scss">
+
+</style>
