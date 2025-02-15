f01e8cca166378792599887850b3df8ebcbaf5b0
kurudoqqe <vadim.firedog@gmail.com>
2025-02-15 18:25:00 +0300

    add client reviews

--- src/routes/_parts/ClientReview.svelte
+++ src/routes/_parts/ClientReview.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    import Star from "$ui-kit/icons/Star.svelte";
+    import EmpyStar from "$ui-kit/icons/EmpyStar.svelte";
+
     type Props = {
         title: string,
         description: string,
         author: string,
         authorImage: string,
-        rating: number,
     }
 
     const {
@@ -12,21 +14,81 @@
         description,
         author,
         authorImage,
-        rating
     }: Props = $props()
 
 </script>
 
 <div class="review-container">
-    <h1>{title}</h1>
-    <p>{description}</p>
+    <div class="review-content">
+        <h1 class="title-1">{title}</h1>
+        <p class="body-text-2">{description}</p>
+    </div>
     <div class="author">
         <img src={authorImage} alt="">
-        <p>{author}</p>
-        {rating}
+        <div>
+            <h5 class="title-3">{author}</h5>
+            <span>
+                <Star size="sm" type="primary"/>
+                <Star size="sm" type="primary"/>
+                <Star size="sm" type="primary"/>
+                <Star size="sm" type="primary"/>
+                <EmpyStar size="sm" type="primary"/>
+            </span>
+        </div>
     </div>
 </div>
 
 <style lang="scss">
+  @use "sass:map";
+  @use "$lib/ui/env";
 
+    .review-container {
+      padding: 1.5rem;
+
+      border: 1px solid rgba(map.get(env.$color, primary), .1);
+      border-radius: 12px;
+
+      height: 100%;
+
+      .review-content {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        > p {
+          line-height: 25.6px;
+        }
+      }
+
+      .author {
+        padding: 2rem 0;
+
+        display: flex;
+        align-items: center;
+        gap: .75rem;
+
+        > img {
+          width: 64px;
+          height: 64px;
+          object-fit: cover;
+
+          border-radius: 8px;
+        }
+
+        > div {
+          display: flex;
+          flex-direction: column;
+          gap: 4px;
+
+          > h5 {
+            font-weight: 700;
+            line-height: 25.6px;
+          }
+
+          > span {
+            display: flex;
+          }
+        }
+      }
+    }
 </style>
--- src/routes/_parts/ClientsSection.svelte
+++ src/routes/_parts/ClientsSection.svelte
@@ -1,21 +1,102 @@
 <script lang="ts">
-
-import ClientReview from "./ClientReview.svelte";
+    import ClientImage1 from "$lib/assets/images/clients/1.png"
+    import ClientImage2 from "$lib/assets/images/clients/2.png"
+    import ClientImage3 from "$lib/assets/images/clients/3.png"
+    import ClientReview from "./ClientReview.svelte";
 </script>
+
+<svelte:head>
+    <link rel="preload" as="image" href={ClientImage1} />
+    <link rel="preload" as="image" href={ClientImage2} />
+    <link rel="preload" as="image" href={ClientImage3} />
+</svelte:head>
 
 <section class="page-container">
     <h2>Что говорят о нас наши клиенты</h2>
-    <ClientReview title="«Огромный выбор врачей, большой выбор‥»."
-                  description="Самый продуманный и постоянно развивающийся сервис по поиску вр чей и онлайн записи к ним.
+    <div class="reviews">
+        <div class="left-comment">
+            <ClientReview title="«Огромный выбор врачей, большой выбор‥»."
+                          description="Самый продуманный и постоянно развивающийся сервис по поиску вр чей и онлайн записи к ним.
                    Есть все популярные клиники и даже государственные больницы,
                     много услуг, отзывы про врачей и негативные и позитивные —
                      можно составить своё сравнительно объективное мнение о враче и примерно
                       представлять что тебя ждёт."
-                  author="Ирина Смирнова" authorImage="" rating=4/>
+                          author="Ирина Смирнова" authorImage={ClientImage1} />
+        </div>
+        <div class="left-comment">
+            <ClientReview title="«Огромный выбор врачей, большой выбор‥»."
+                          description="Самый продуманный и постоянно развивающийся сервис по поиску вр чей и онлайн записи к ним.
+                   Есть все популярные клиники и даже государственные больницы,
+                    много услуг, отзывы про врачей и негативные и позитивные —
+                     можно составить своё сравнительно объективное мнение о враче и примерно
+                      представлять что тебя ждёт."
+                          author="Ирина Смирнова" authorImage={ClientImage2} />
+        </div>
+        <div class="right-comment">
+            <ClientReview title="«Огромный выбор врачей, большой выбор‥»."
+                          description="Самый продуманный и постоянно развивающийся сервис по поиску вр чей и онлайн записи к ним.
+                   Есть все популярные клиники и даже государственные больницы,
+                    много услуг, отзывы про врачей и негативные и позитивные —
+                     можно составить своё сравнительно объективное мнение о враче и примерно
+                      представлять что тебя ждёт."
+                          author="Ирина Смирнова" authorImage={ClientImage3} />
+        </div>
+        <div class="all-reviews">
+            <h4 class="title-1">Посмотреть все отзывы</h4>
+        </div>
+    </div>
+
 </section>
 
 <style lang="scss">
+  @use "sass:map";
+  @use "$lib/ui/env";
+
     section {
-      margin-top: 192px;
+      padding-top: 192px;
+
+      .reviews {
+        display: grid;
+        grid-template-columns: repeat(12, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+        grid-auto-flow: column;
+        column-gap: 32px;
+        row-gap: 32px;
+
+        margin-top: 64px;
+
+        > div {
+          grid-column: span 4;
+          grid-row: span 3;
+
+          &.left-comment {
+            grid-column: span 8;
+          }
+
+          &.right-comment {
+            grid-row: span 4;
+          }
+        }
+
+        .all-reviews {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
+          grid-row: span 2;
+
+          padding: 1.5rem;
+
+          background-color: map.get(env.$color, primary);
+
+          border-radius: 12px;
+
+          cursor: pointer;
+
+          > h4 {
+            color: map.get(env.$color, secondary);
+          }
+        }
+      }
     }
 </style>
