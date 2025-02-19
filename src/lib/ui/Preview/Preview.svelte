b1d5f35f04eae9ea4d7060128b46b4e210c28772
kurudoqqe <vadim.firedog@gmail.com>
2025-02-19 22:33:28 +0300

    add preview component

---
+++ src/lib/ui/Preview/Preview.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import type {Snippet} from "svelte";
+
+    type Props = {
+        children: Snippet,
+        title: string;
+        image: string;
+        height: number;
+    }
+
+    let {
+        children,
+        title,
+        image,
+        height,
+    }: Props = $props()
+
+</script>
+
+<div class="preview" style:--img={`url(${image})`} style:--height={height}>
+    <div class="preview-content">
+        <h1>{title}</h1>
+        {@render children?.()}
+    </div>
+</div>
+
+<style lang="scss">
+  @use "sass:map";
+  @use "$lib/ui/env";
+
+  .preview {
+    display: flex;
+    align-items: center;
+
+    background-image: linear-gradient(to right, map.get(env.$bg-color, primary) 45%, transparent 60%), var(--img);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: right;
+
+    width: 100%;
+    height: 610px;
+
+    padding: 17.5rem 8.5rem;
+
+    border: 1px solid rgba(map.get(env.$color, primary), .1);;
+    border-radius: 12px;
+
+    .preview-content {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+
+      width: 650px;
+
+      > h1 {
+        line-height: 70.4px;
+      }
+    }
+  }
+</style>
--- src/routes/doctors/list/+page.svelte
+++ src/routes/doctors/list/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-
+    import Preview from "$ui-kit/Preview/Preview.svelte";
+    import DoctorListPreview from "../../_parts/assets/doctor-list-preview.png?enhanced&format=webp"
 </script>
 
-<main class="page-container">
+<svelte:head>
+    <link rel="preload" as="image" href={DoctorListPreview.img.src} />
+</svelte:head>
 
+<main class="page-container">
+    <Preview title="Неврологи в Москве" image={DoctorListPreview.img.src} height={1000}>
+        <p class="body-text-1">Невролог — врач, который диагностирует и проводит лечение заболеваний
+            центральной и периферической нервной системы — спинного и головного мозга,
+            а также нервных волокон.</p>
+    </Preview>
 </main>
+
+<style lang="scss">
+    .page-container {
+      .body-text-1 {
+        line-height: 28.8px;
+      }
+    }
+</style>
