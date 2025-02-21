4548d1b3f14506a3e47bb369ce8b2fe202c22c31
EmOtIoN <sunundershadow@gmail.com>
2025-02-21 14:08:10 +0300

    Page preview rollback

--- src/routes/doctors/list/+layout.svelte
+++ src/routes/doctors/list/+layout.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
-  import {setContext} from "svelte"
-  import Preview from "$ui-kit/Preview/Preview.svelte"
   import Breadcrumbs from "$ui-kit/Breadcrumbs/Breadcrumbs.svelte"
-
-  let previewURI = $state('')
-
-  setContext('setPreview', (uri: string) => {
-      console.log(uri)
-      previewURI = uri
-  })
+  import type {Snippet} from "svelte"
 
   let breadcrumbs = [
       {
@@ -20,27 +12,26 @@
           href: '/doctors/list'
       }
   ]
-</script>
 
-<svelte:head>
-  <link rel="preload" as="image" href={previewURI} />
-</svelte:head>
+  type Props = {
+      children: Snippet
+  }
+
+  const {
+      children,
+  }: Props = $props()
+</script>
 
 <main class="page-container">
   <div class="breadcrumbs">
     <Breadcrumbs list={breadcrumbs}/>
   </div>
 
-  <Preview title="Неврологи в Москве" image={previewURI} height={1000}>
-    <p class="body-text-1">Невролог — врач, который диагностирует и проводит лечение заболеваний
-      центральной и периферической нервной системы — спинного и головного мозга,
-      а также нервных волокон.</p>
-  </Preview>
+  {@render children?.()}
 </main>
 
-<slot />
 
-<style lang="scsss">
+<style lang="scss">
   .breadcrumbs {
       margin-bottom: 40px;
   }
--- src/routes/doctors/list/+page.svelte
+++ src/routes/doctors/list/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import DoctorListPreview from "../../_parts/assets/doctor-list-preview.png?enhanced&format=webp"
-    import {getContext} from "svelte"
+    import Preview from "$ui-kit/Preview/Preview.svelte";
 
-    let setPreview = getContext('setPreview')
-    setPreview(DoctorListPreview.img.src)
 </script>
 
+<svelte:head>
+  <link rel="preload" as="image" href={DoctorListPreview.img.src} />
+</svelte:head>
+
+<Preview title="Неврологи в Москве" image={DoctorListPreview.img.src} height={1000}>
+  <p class="body-text-1">Невролог — врач, который диагностирует и проводит лечение заболеваний
+    центральной и периферической нервной системы — спинного и головного мозга,
+    а также нервных волокон.</p>
+</Preview>
 
 
 <style lang="scss">
