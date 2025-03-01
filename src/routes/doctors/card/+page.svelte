44269eb3c468d0aa7874af2bbe5e132825b06313
kurudoqqe <vadim.firedog@gmail.com>
2025-03-01 20:21:08 +0300

    add breadcrumbs to doctors/card

--- src/routes/doctors/card/+page.svelte
+++ src/routes/doctors/card/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
+  import Breadcrumbs from "$ui-kit/Breadcrumbs/Breadcrumbs.svelte";
 
+  let breadcrumbs = [
+      {
+          title: 'Главная',
+          href: '/'
+      },
+      {
+          title: 'Врачи',
+          href: '/'
+      },
+      {
+          title: 'Взрослый врач Невролог',
+          href: '/'
+      },
+      {
+          title: 'Горбунов Алексей Викторович',
+          href: ''
+      }
+  ]
 </script>
 
 <main class="page-container">
-
+  <Breadcrumbs list={breadcrumbs} />
 </main>
 
 <style lang="scss">
