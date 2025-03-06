56df79357f0b90b54fd15e5b1ec1290ca82d3be8
kurudoqqe <vadim.firedog@gmail.com>
2025-03-06 21:13:35 +0300

    initialize new page /library/diseases

---
+++ src/routes/library/diseases/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Breadcrumbs from "$ui-kit/Breadcrumbs/Breadcrumbs.svelte";
+
+  let breadcrumbs = [
+      {
+          title: 'Главная',
+          href: '/'
+      },
+      {
+          title: 'Библиотека',
+          href: '/'
+      },
+      {
+          title: 'Заболевания',
+          href: ''
+      }
+  ]
+</script>
+
+<main class="page-container">
+  <div class="breadcrumbs">
+    <Breadcrumbs list={breadcrumbs}/>
+  </div>
+
+</main>
+
+<style lang="scss">
+
+</style>
